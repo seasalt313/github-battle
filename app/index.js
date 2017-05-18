1faebd51cfa96dbc31e8252d94420a441e4004f1
Mariam Qasim <mqfanhero@Mariams-MacBook-Pro.local>
2017-05-18 10:22:46 -0400

    removed dist from gitignore

--- app/index.js
+++ app/index.js
@@ -1,7 +1,12 @@
+// import React, { Component } from 'react';
+// import ReactDom from 'react-dom';
+// import './index.css';
+
 var React = require('react');
-var ReactDOM = require('react-dom');
+var ReactDom = require('react-dom');
 require('./index.css');
 
+//component will have state, and will also have lifestyle events, and will have UI
 class App extends React.Component {
   render() {
     return (
