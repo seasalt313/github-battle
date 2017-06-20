84e472eea9e84305649701eedfcb83769ec7ef84
Mariam Qasim <mariam@fanhero.com>
2017-06-20 12:10:29 -0400

    fixed bracket placement

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -45,8 +45,7 @@ function RepoGrid(props) {
                 <a href={repo.html_url}>{repo.name}</a>
               </li>
               <li>@{repo.owner.login}</li>
-              <li>{repo.stargazers_count}
-                stars</li>
+              <li>{repo.stargazers_count} stars</li>
             </ul>
           </li>
         )
